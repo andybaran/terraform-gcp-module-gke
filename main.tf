7538cd5396aace6d8072e7d993054b0846ad00df
Andy Baran <andy.baran@hashicorp.com>
2020-09-20 14:59:20 -0400

    clean-up

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
+terraform {
+    required_version = ">= 0.12.0"
+    required_providers {
+        google = "~> 3.24.0"
+    }
+}
+
+# Presumably our admins created a project for us using TFE and we're going to get info about that project from the resulting workspace.
 data "terraform_remote_state" "project" {
   backend = "remote"
-
   config = {
     hostname = var.remote-hostname
     organization = var.organization-name
@@ -17,6 +24,7 @@ provider "google" {
   zone = var.zone
 }
 
+# BigQuery for GKE logs? Yes please!  Let's make a dataset
 resource "google_bigquery_dataset" "cluster-usage-dataset" {
     dataset_id = var.bq-cluster-usage-dataset
     friendly_name = var.bq-cluster-usage-dataset
@@ -24,19 +32,22 @@ resource "google_bigquery_dataset" "cluster-usage-dataset" {
     location = "US"
 }
 
+# Lets make our lives easy and give the service account from our remote_state GKE cluster admin access
 resource "google_project_iam_member" "gke_cluster_admin" {
     project = var.gcloud_project
     role = "roles/container.clusterAdmin"
     member = "serviceAccount:${data.terraform_remote_state.project.outputs.service_account_email}"
 }
 
-
+# Let's get even crazier and give the service account from our remote_state container admin access
 resource "google_project_iam_member" "gke_container_admin" {
     project = var.gcloud_project
     role = "roles/container.admin"
     member = "serviceAccount:${data.terraform_remote_state.project.outputs.service_account_email}"
 }
 
+# Finally done igonoring all infosec best practices (that's OK this IS NOT in production, right?!)
+# ... And now we can create a GKE cluster
 resource "google_container_cluster" "primary" {
 
   depends_on = [google_bigquery_dataset.cluster-usage-dataset,google_project_iam_member.gke_cluster_admin, google_project_iam_member.gke_container_admin]
@@ -49,10 +60,11 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  network= "provisioning-vpc"
+  network= data.terraform_remote_state.project.outputs.primary_net
 
   master_auth {
 
+    # This is going to make deploying workloads to this cluster significantly easier
     client_certificate_config {
       issue_client_certificate = true
     }
